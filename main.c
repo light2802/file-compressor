97feb9d21514ac552751602114aef361ce052a02
Aarya Chaumal <aarya.chaumal@gmail.com>
2021-03-01 22:36:18 +0530

    dirent.h

--- main.c
+++ main.c
@@ -4,12 +4,24 @@ Author: Aarya Chaumal
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 #include "read_directory.h"
 
 //Problem: Main File for Project zip
 
-int main()
+int main(int argc, char** argv)
 {
-
+    DIR *folder;
+    char* loaction;
+    int compression_type=0;
+    for(int i=1;i<argc;i++)
+    {
+        if(strcmp(argv[i],"-a"))
+        {
+            strcpy(loaction,".");
+        }
+    }
+    folder = opendir(".");
     return 0;
 }
