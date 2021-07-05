d229d49510c6463a29a033a33304682ab0afca6a
light2802 <aarya.chaumal@gmail.com>
2021-07-05 20:51:44 +0530

    organised

--- automate_lorem.sh
+++ automate_lorem.sh
@@ -1,10 +1,34 @@
-#!/usr/bin/env perl
-i=1
-while [ $i -lt 10000000000 ]
+#!/bin/bash
+
+input=10000
+count=5
+output="results.csv"
+type="exe"
+while getopts i:c:o:t: flag
 do
-    lorem -w $i > random.txt
-    ./aar.exe -e random.txt
-    rm random.txt
-    rm random.txt.aar
-    i=`expr $i + 10`
+    case "${flag}" in
+        u) input=${OPTARG};;
+        a) count=${OPTARG};;
+        f) output=${OPTARG};;
+        t) type=${OPTARG};;
+    esac
 done
+
+echo "START................................"
+echo "Press Ctrl + C to end midway........."
+i=1
+
+
+lorem -w $input > random.txt
+
+while [ $i -lt $count ]
+do
+    ./aar.exe -e random.txt -r $output
+    rm random.txt
+    mv random.txt.aar random.txt
+    i=`expr $i + 1`
+done
+
+rm random.txt
+
+echo "END.................................."
